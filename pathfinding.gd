cfccfc44b02f8f229620202686a461232c7e9882
r0nk <r0nk@simplecpu.com>
2023-12-05 20:34:34 -0600

    added basic right click movement.

---
+++ pathfinding.gd
@@ -0,0 +1,18 @@
+extends Node
+
+var destination = Vector3(5,2,5)
+var speed = 5
+var p
+
+func _ready():
+	p = get_parent()
+
+func _process(delta):
+	if(p.position.distance_to(destination) == 0.0):
+		return
+	p.velocity = (destination-p.position).normalized()*speed
+	p.move_and_slide()
+	if(p.position.distance_to(destination) < 0.1):
+		p.position=destination
+
+
