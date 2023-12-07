726a56e8eb57acf7f2c3ff52aa2d3ebb8b88691d
r0nk <r0nk@simplecpu.com>
2023-12-06 22:03:32 -0600

    barracks

--- barracks.gd
+++ barracks.gd
@@ -1,4 +1,4 @@
-extends Sprite2D
+extends StaticBody3D
 
 @onready var creep = load("res://creep.tscn")
 
@@ -7,20 +7,12 @@ var timer = 0
 func _on_timer_timeout():
 	for i in range(4):
 		var c = creep.instantiate()
-		c.position = position + Vector2(randf_range(-1,1),randf_range(-1,1))
+		c.position = $spawn_point.position + Vector3(randf_range(-1,1),0,randf_range(-1,1))
 		add_sibling(c)
-		if name == "bad_barracks":
-			c.get_node("acquisition").team="bad"
-			c.get_node("acquisition").target_name="rad_tower"
-			c.get_node("sprite").self_modulate="#FF0000"
-		if name == "rad_barracks":
-			c.get_node("acquisition").team="rad"
-			c.get_node("acquisition").target_name="bad_tower"
-			c.get_node("sprite").self_modulate="#00FF00"
 
 func _process(delta):
 	timer-=delta
 	if (timer < 0):
 		_on_timer_timeout()
-		timer=30
+		timer=15
 
