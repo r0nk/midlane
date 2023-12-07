ac5c16d01d421bab7ac141e16ef8edda59af4a43
r0nk <r0nk@simplecpu.com>
2023-12-06 22:28:35 -0600

    start of move for tower to 3d

--- tower.gd
+++ tower.gd
@@ -1,22 +1,5 @@
-extends CharacterBody2D
+extends StaticBody3D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-var max_health = 2000
-var health = max_health
-@export var team = "rad"
-
-func _on_acquisition_body_entered(_body):
-	$acquisition.process_targeting()
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	$acquisition.team = team
-	if(team=="rad"):
-		$acquisition.target_name = "bad_tower"
-		$acquisition.process_targeting()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$bar.value=100*health/max_health
+	$model/turret.look_at($"../unit".position)
+
